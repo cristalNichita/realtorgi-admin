f07cfd80644b9dad69235fcdae4869818a97c713
cristalNichita <kristal.nikita@outlook.com>
2021-09-17 12:16:11 +0300

    first commit

---
+++ resources/views/emails/auctionConfirm.blade.php
@@ -0,0 +1,5 @@
+Уважаемый Пользователь площадки realtorgi.by, сообщаем что администратор подтвердил вашу заявку на участие по лоту №{{ $lot_number }}, <br>
+С уважением, команда realtorgi.by <br>
+Телефон: <a href="tel:+375255498622">+375 (25) 549-86-22</a> <br>
+Email: <a href="mailto:auction@expertiza.by">auction@expertiza.by</a> <br>
+<a href="https://realtorgi.by" target="_blank">https://realtorgi.by</a>
